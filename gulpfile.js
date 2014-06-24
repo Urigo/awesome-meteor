52de7a3ab433d94b0d56b5881c4682d8a65d8515
Gilles Fabio <gilles@gillesfabio.com>
2014-06-24 16:18:05 +0200

    Add task to find duplicates.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 'use strict';
 
+var fs       = require('fs');
 var path     = require('path');
 var express  = require('express');
+var _        = require('lodash');
 var gulp     = require('gulp');
 var concat   = require('gulp-concat');
 var minify   = require('gulp-minify-css');
@@ -10,6 +12,8 @@ var deploy   = require('gulp-gh-pages');
 var swig     = require('swig');
 var build    = require('./build');
 
+// Assets
+// -----------------------------------------------------------------------------
 gulp.task('assets:css', function() {
   return gulp.src([
       'bower_components/normalize-css/normalize.css',
@@ -29,13 +33,27 @@ gulp.task('assets:images', function() {
 
 gulp.task('assets', ['assets:css', 'assets:images']);
 
-gulp.task('lint:json', function() {
+// Bookmarks
+// -----------------------------------------------------------------------------
+gulp.task('bookmarks:check:json', function() {
   return gulp.src(['src/bookmarks.json'])
     .pipe(jsonlint())
     .pipe(jsonlint.reporter());
 });
 
-gulp.task('build:metalsmith', ['lint:json'], function(done) {
+gulp.task('bookmarks:check:duplicates', ['bookmarks:check:json'], function() {
+  var json  = JSON.parse(fs.readFileSync(path.join(__dirname, 'src', 'bookmarks.json')));
+  var items = _.flatten(_.values(json));
+  var uniqs = _.uniq(items, function(b) { return b.url; });
+  var dups  = _.filter(uniqs, function(b) { return _.filter(items, {url: b.url}).length >= 2; });
+  if (dups.length) throw new Error('Found duplicated URLs: \n' + _.pluck(dups, 'url').join('\n'));
+});
+
+gulp.task('bookmarks', ['bookmarks:check:duplicates']);
+
+// Build
+// -----------------------------------------------------------------------------
+gulp.task('build:metalsmith', ['bookmarks'], function(done) {
   build(function(err){
     if (err) return err;
     console.log('Metalsmith building... Done.');
@@ -48,11 +66,15 @@ gulp.task('build', ['build:metalsmith', 'assets'], function() {
     .pipe(gulp.dest('public'));
 });
 
+// Deploy
+// -----------------------------------------------------------------------------
 gulp.task('deploy', ['build'], function() {
   return gulp.src('./public/**/*')
     .pipe(deploy());
 });
 
+// Serve
+// -----------------------------------------------------------------------------
 gulp.task('serve', ['watch', 'build'], function() {
   var port = process.env.NODE_PORT || 3000;
   var app = express();
@@ -63,6 +85,8 @@ gulp.task('serve', ['watch', 'build'], function() {
   console.log('Server running on localhost:%d...', port);
 });
 
+// Watch
+// -----------------------------------------------------------------------------
 gulp.task('watch', function() {
   gulp.watch([
     'assets/**/*',
@@ -71,4 +95,6 @@ gulp.task('watch', function() {
   ], ['build']);
 });
 
+// Default
+// -----------------------------------------------------------------------------
 gulp.task('default', ['serve']);
